156f94c0e4df8e8c235a7856ce6f10787681d53b
Mariyam Biju <130202520+mariyambj@users.noreply.github.com>
2026-01-23 05:48:56 +0530

    Time: 83 ms (78.13%) | Memory: 36.8 MB (18.26%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        n=len(nums)
+        window_size=2*k+1
+        window_sum=0
+        result=[-1]*n
+        if n < window_size :
+            return result
+        for i in range(n):
+            window_sum+=nums[i]
+            if i-window_size>=0:
+                window_sum-=nums[i-window_size]
+            if i>=window_size-1:
+                result[i-k]=window_sum//window_size
+        return result
