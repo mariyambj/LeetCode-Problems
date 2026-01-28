aafcbca3a5a004ba45b041a616b450e050922545
Mariyam Biju <130202520+mariyambj@users.noreply.github.com>
2026-01-28 06:11:56 +0530

    Time: 3 ms (89.21%) | Memory: 19.3 MB (20.61%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.py
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -4,9 +4,10 @@ class Solution:
         for i in range(1,len(s)):
             if s[i]==s[i-1]:
                 count+=1
+                max_count=max(count,max_count)
             else:
                 count=1
-            max_count=max(count,max_count)
+            
         return max_count
 
 
