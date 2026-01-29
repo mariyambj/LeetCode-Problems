73ec13d40aeed7dcac34affba751f86f8fb82213
Mariyam Biju <130202520+mariyambj@users.noreply.github.com>
2026-01-30 05:10:54 +0530

    Time: 2155 ms (5.04%) | Memory: 58.4 MB (11.36%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        if len(s)==0 and len(s) < k:
+            return 0
+        count={}
+        for i in range(len(s)):
+            if s[i] not in count:
+                count[s[i]]=1
+            else:
+                count[s[i]]+=1
+        for i,char in enumerate(s):
+            if count[char]<k:
+                left=self.longestSubstring(s[:i],k)
+                right=self.longestSubstring(s[i+1:],k)
+                return max(left, right)
+        return len(s)
+        
